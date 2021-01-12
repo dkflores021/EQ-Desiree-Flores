1e50dbf35859ae49b1ef14edc4b9124dc803e1f1
Desiree  Flores <desireek.flores@gmail.com>
2021-01-12 23:28:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print ("Hello! I am a chatbot and my name is Jarvis!")
+name = input("What is your name?\n")
+print ("Nice to meet you, %s!" %name)
+
+print("To exit the chat, enter \"Goodbye Jarvis!\" ")
+
+def subject_says (emotion):
+  print ("How are you feeling today?")
+  if subject_says:
+    else: 
+  subject()
+# how has your day been today or 
+#how are you feeling so far today: 
+# - alright | - happy | - sad | 
+# - happy = whats made you happy today? any good news?
+# - sad = i'm sorry you're not feeling so well. would you like to explain how you feel? (could put a number limit on how much can be typed)
+# or ask if the subject would like to hear a joke or put a link to cool math games
+#also give the number or link to the national suicide hotline if they feel super down. 
